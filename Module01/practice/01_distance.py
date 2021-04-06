f689f2d3a72816decba7057a5ac987c8cc13d62c
Michael Nikitenko <xm4dn355x@gmail.com>
2021-04-06 14:37:22 +0500

    practice. find distance between two points

---
+++ Module01/practice/01_distance.py
@@ -0,0 +1,26 @@
+# Дано две точки на координатной плоскости
+
+
+from math import sqrt
+
+
+class Point:
+    """Точка в двоичкойсистеме координат"""
+    def __init__(self, x: int, y: int):
+        self.x = x
+        self.y = y
+
+
+def distance(p1: Point, p2: Point) -> float:
+    """Расстояние между двумя точками"""
+    return round(sqrt((p2.x - p1.x) ** 2 + (p2.y - p1.y) ** 2), 2)
+
+
+if __name__ == '__main__':
+    point1 = Point(2, 4)
+    point2 = Point(5, -2)
+
+    # Задание: Найдите расстояние между этими точками. Реализовав и используя функцию distance()
+    res = distance(point1, point2)
+
+    print(f"Расстояние между точками = {res}")
